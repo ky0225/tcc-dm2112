2d632dadbbe1104608b1b45d591742aac7f7b35c
yamamoto <yamamoto.k@marietta.co.jp>
2021-12-24 15:21:43 +0900

    [Add]Model作成

---
+++ tcc-dm/app/Models/DM_Rariry.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class DM_Rariry extends Model
+{
+    use HasFactory;
+}
